7be612ecc2a35a68fb65ef23cf2b56db6cffd956
Timur Khusainov <timur.khusainov@data-brainstorm.com>
2019-06-13 09:27:39 +0200

    added dynamic schema for tables

--- broccoli_mdm/models.py
+++ broccoli_mdm/models.py
@@ -1,10 +1,27 @@
 from broccoli_mdm import db
 
 
-class Countries(db.Model):
+#class Countries(db.Model):
+#    id = db.Column(db.Integer, primary_key=True)
+#    country_name = db.Column(db.String(64), index=True, unique=True)
+#    iso_code = db.Column(db.String(120), index=True, unique=True)
+#    currency_code = db.Column(db.String(128))
+
+Column = db.Column
+
+class tables(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    country_name = db.Column(db.String(64), index=True, unique=True)
-    iso_code = db.Column(db.String(120), index=True, unique=True)
-    currency_code = db.Column(db.String(128))
+    schema = db.Column(db.String, unique=True) 
+    name = db.Column(db.String)
+    sql_alchemy_definition = db.Column(db.String)
+    is_active = db.Column(db.Integer)
 
 
+attr_dict = {
+        'id': Column(db.Integer, primary_key=True),
+	    'country_name': Column(db.String(64), index=True, unique=True),
+        'iso_code' : Column(db.String(120), index=True, unique=True),
+        'currency_code' : Column(db.String(128))}
+
+
+Countries = type('Countries', (db.Model,), attr_dict)
