fa73989c43ffa2804cbf4517fc0383e546f4ef3f
Timur Khusainov <timur.khusainov@data-brainstorm.com>
2019-06-21 17:07:07 +0200

    added binging for multiple database connections

--- broccoli_mdm/init_models.py
+++ broccoli_mdm/init_models.py
@@ -1,5 +1,8 @@
-from broccoli_mdm.models import tables
-from broccoli_mdm import db
+from broccoli_mdm.models import tables, connections
+from broccoli_mdm import db, Config, app
+
+for con in connections.query.all():
+    app.config.SQLALCHEMY_BINDS = {con.schema : con.connection_string}
 
 
 db.reflect()
@@ -7,7 +10,9 @@ db.reflect()
 d = dict()
 for row in tables.query.all():
     if row.is_active == 1:
-        s = "class %s(db.Model): __tablename__ = '%s'" % (row.name.lower(), row.name.lower())
+        s = """class %s(db.Model):
+                __bind_key__ = 'Test'
+                __tablename__ = '%s'""" % (row.name.lower(), row.name.lower())
         exec(s)
         d[row.name] = eval(row.name.lower())
 
