0b9f86903d365b13e6ca7061e82ce22e7b4b6857
Timur Khusainov <timur.khusainov@data-brainstorm.com>
2019-06-21 18:50:27 +0200

    added dynamic binding. Works good

--- broccoli_mdm/init_models.py
+++ broccoli_mdm/init_models.py
@@ -1,6 +1,10 @@
 from broccoli_mdm.models import tables, connections
-from broccoli_mdm import db, Config, app
+from broccoli_mdm import db, app
 
+app.config["SQLALCHEMY_BINDS"] = dict()
+
+for con in connections.query.all():
+    app.config["SQLALCHEMY_BINDS"][con.schema] = con.connection_string
 
 
 db.reflect()
@@ -9,8 +13,8 @@ d = dict()
 for row in tables.query.all():
     if row.is_active == 1:
         s = """class %s(db.Model):
-                __bind_key__ = 'Test'
-                __tablename__ = '%s'""" % (row.name.lower(), row.name.lower())
+                __bind_key__ = '%s'
+                __tablename__ = '%s'""" % (row.name.lower(), row.schema, row.name.lower())
         exec(s)
         d[row.name] = eval(row.name.lower())
 
