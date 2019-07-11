62bc0cc06ef5867d7a0bf76a18d04bc065be7795
Timur Khusainov <timur.khusainov@data-brainstorm.com>
2019-07-11 09:55:41 +0200

    fix prev

--- run_production.sh
+++ run_production.sh
@@ -1,4 +1,4 @@
 #get changes and reload server
 pkill flask
-nohup flask run --reload --with-threads --port 7890 -h 0.0.0.0 2>&1 &
+nohup flask run --reload --with-threads --port 80 -h 0.0.0.0 2>&1 &
 exit o
