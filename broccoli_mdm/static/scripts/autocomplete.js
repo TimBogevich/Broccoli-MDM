2c287facddf7e6893420b96c33bbaa7bfc2f1b71
Timur Khusainov <timur.khusainov@data-brainstorm.com>
2019-07-29 20:55:10 +0200

    added autocomplete for filter

---
+++ broccoli_mdm/static/scripts/autocomplete.js
@@ -0,0 +1,39 @@
+function split( val ) {
+    return val.split(" ");
+  }
+  function extractLast( term ) {
+    return split( term ).pop();
+  }
+
+
+  $( "#filter" )
+    // don't navigate away from the field on tab when selecting an item
+    .on( "keydown", function( event ) {
+      if ( event.keyCode === $.ui.keyCode.TAB &&
+          $( this ).autocomplete( "instance" ).menu.active ) {
+        event.preventDefault();
+      }
+    })
+    .autocomplete({
+      minLength: 0,
+      source: function( request, response ) {
+        // delegate back to autocomplete, but extract the last term
+        response( $.ui.autocomplete.filter(
+            tableHeader.concat(["==", "in", "or", "and", ">", "<", "!="]), extractLast( request.term ) ) );
+      },
+      focus: function() {
+        // prevent value inserted on focus
+        return false;
+      },
+      select: function( event, ui ) {
+        var terms = split( this.value );
+        // remove the current input
+        terms.pop();
+        // add the selected item
+        terms.push( ui.item.value );
+        // add placeholder to get the comma-and-space at the end
+        terms.push( "" );
+        this.value = terms.join( " " );
+        return false;
+      }
+    });
